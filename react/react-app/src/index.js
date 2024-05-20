20b945ef88068451ab65b041aa1ed8f677fbab77
AditiKotkar <150508095+AditiKotkar@users.noreply.github.com>
2024-05-20 05:09:49 -0700

    Update index.js

--- react/react-app/src/index.js
+++ react/react-app/src/index.js
@@ -6,6 +6,16 @@ import reportWebVitals from './reportWebVitals';
 import StudentLogin from './components/studentlogin'; 
 import StudentDashbord from './components/StudentDashbord';
 import Profilepage from './components/Profilepage';
+import Homework from './components/homework/Homework';
+import Mainsub from './components/homework/Mainsub';
+import Syllabus from './components/syllabus/Syllabus';
+import Subject from './components/syllabus/Subject';
+import RomanNumerals from './components/syllabus/RomanNumerals';
+import Attendancecal from './components/Attendence/Attendancecal';
+import Feedback from './components/Feedback/Feedback';
+import Report from './components/Report/Report';
+import Results from './components/Results/Results';
+import 'react-calendar/dist/Calendar.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,6 +24,15 @@ root.render(
       <Route path="/" element={<StudentLogin />} /> 
       <Route path='student' element={<StudentDashbord />} />
       <Route path='profile' element={<Profilepage />} />
+      <Route path="home" element={<Homework />} />
+      <Route path="mainsub" element={<Mainsub />} />
+      <Route path="Syllabus" element={<Syllabus />} />
+      <Route path="Subject" element={<Subject />} />
+      <Route path="RomanNumerals" element={<RomanNumerals />} />
+      <Route path="Attendancecal" element={<Attendancecal />} />
+      <Route path="Feedback" element={<Feedback/>}/>
+      <Route path="Report" element={<Report/>}/>
+      <Route path="Results" element={<Results/>}/>
     </Routes>
   </BrowserRouter>
 );
